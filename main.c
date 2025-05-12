f5d2dcb8c64de7952ce2f4e1f58a0ea1d45e9cc9
Lei Andrei Domaoal <leiandreiedomaoal@iskolarngbayan.pup.edu.ph>
2025-05-12 21:43:03 +0800

    Update main.c

--- main.c
+++ main.c
@@ -6,16 +6,16 @@
 struct node
 {
   int data;
-  struct node *next
+  struct node *next;
 };
 
 int main() {
 
   struct node *head, *middle, *last;
 
-  *head = malloc(sizeof(struct node));
-  *middle = malloc(sizeof(struct node));
-  *last = malloc(sizeof(struct node));
+  head = malloc(sizeof(struct node));
+  middle = malloc(sizeof(struct node));
+  last = malloc(sizeof(struct node));
 
   head -> data = 100;
   middle -> data = 200;
