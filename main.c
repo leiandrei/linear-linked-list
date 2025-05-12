e0bad91c251158e4d34f7756bc5735ef6dc0c110
Lei Andrei Domaoal <leiandreiedomaoal@iskolarngbayan.pup.edu.ph>
2025-05-12 21:39:19 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// simple implementation of linear linked lists
+
+struct node
+{
+  int data;
+  struct node *next
+};
+
+int main() {
+
+  struct node *head, *middle, *last;
+
+  *head = malloc(sizeof(struct node));
+  *middle = malloc(sizeof(struct node));
+  *last = malloc(sizeof(struct node));
+
+  head -> data = 100;
+  middle -> data = 200;
+  last -> data = 300;
+
+  head -> next = middle;
+  middle -> next = last;
+  last -> next = NULL;
+
+  struct node *temp = head;
+
+  printf("Data: ");
+  while (temp != NULL) 
+  {
+      printf("%d ", temp -> data);
+      temp = temp -> next;
+  }
+  printf("null");
+  return 0;
+}
+
